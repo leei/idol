166156e9e2003559713d88ef49571ca8f9a7ec74
Lee Iverson <leei@ece.ubc.ca>
2011-03-21 15:34:28 -0700

    Save and reload redis object

--- test/redis.vows.js
+++ test/redis.vows.js
@@ -34,6 +34,83 @@ suite.addBatch({
       }
     },
 
+    'with a set of attributes': {
+      topic: function(ctor) {
+        return ctor.define('Obj', {
+          kind: 'object',
+          attributes: { a: {}, b: {default: true} }
+        });
+      },
+
+      'can describe those attributes': function(ctor) {
+        assert.deepEqual(ctor.attributes['a'], {});
+        assert.deepEqual(ctor.attributes['b'], {default: true});
+      },
+
+      'when instantiated': {
+        topic: function(ctor) {
+          return new ctor({a: 1});
+        },
+
+        'sets given attributes': function(obj) {
+          assert.equal(obj.a, 1);
+        },
+
+        'sets defaults if not given': function(obj) {
+          assert.equal(obj.b, true);
+        },
+
+        'is not saved': function(obj) {
+          assert.isTrue(obj.isNewRecord());
+        },
+
+        'and saved': {
+          topic: function(obj) {
+            obj.save(this.callback);
+          },
+
+          'returns success': function(err, obj) {
+            //console.log("save success" + (err ? ": " + err : ""));
+            assert.isTrue(! err);
+            assert.isFalse(obj.isNewRecord());
+          },
+
+          'has a numeric ID': function(err, obj) {
+            //console.log("save numeric" + (err ? ": " + err : ""));
+            assert.isNumber(obj.id);
+          },
+
+          'and reloaded': {
+            topic: function(obj, _, ctor) {
+              //console.log("reload " + ctor.name + ":" + obj.id);
+              ctor.load(obj.id, this.callback);
+            },
+
+            'has the same id': function(err, loaded) {
+              var obj = this.context.topics[1];
+              assert.equal(loaded.id, obj.id);
+            },
+
+            'has the same attributes': function(err, loaded) {
+              var obj = this.context.topics[1];
+              assert.deepEqual(obj.attributes, loaded.attributes);
+            },
+
+            'and destroyed': {
+              topic: function(_, obj, _, ctor) {
+                obj.destroy(this.callback);
+              },
+
+              'succeeds': function(err, success) {
+                assert.isTrue(! err);
+                assert.isTrue(success);
+              }
+            }
+          }
+        }
+      }
+    },
+
     teardown: function() {
       client.flushdb();
     }
